ed8c35be8d60bde6065c7a57dd0caf07cd144782
igorGabrielM <igorgabprofissional@gmail.com>
2026-03-28 19:53:51 -0300

    feature: add vercel configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
-bootstrap();
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+export default async (req: any, res: any) => {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const instance = await app.getHttpAdapter().getInstance();
+  return instance(req, res);
+};
