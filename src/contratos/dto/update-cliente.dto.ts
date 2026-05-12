abc5df2ddf6c8f397025becae2a5ac2d23084bda
igorGabrielM <igorgabprofissional@gmail.com>
2026-05-12 20:22:21 -0300

    feat: validacao correta do email na edicao do cliente

--- src/contratos/dto/update-cliente.dto.ts
+++ src/contratos/dto/update-cliente.dto.ts
@@ -1,10 +1,10 @@
-import { IsString, IsOptional, IsEmail } from 'class-validator';
+import { IsString, IsOptional, IsEmail, ValidateIf } from 'class-validator';
 
 export class UpdateClienteDto {
   @IsOptional() @IsString() nome?: string;
   @IsOptional() @IsString() cpf?: string;
   @IsOptional() @IsString() rg?: string;
   @IsOptional() @IsString() telefone?: string;
-  @IsOptional() @IsEmail() email?: string;
+  @IsOptional() @ValidateIf((o) => o.email !== '') @IsEmail() email?: string;
   @IsOptional() @IsString() endereco?: string;
 }
